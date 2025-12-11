c9d3c3be6dc5e99745d61c28d2c11e0a236cadb7
vandevrede <telacaster@gmail.com>
2025-12-10 19:15:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import streamlit as st
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_community.document_loaders import UnstructuredFileLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 
 # IMPORTANT: Replace with your actual OpenAI API key
@@ -60,4 +60,5 @@ if user_input:
     ]
     response = llm.invoke(messages)
     st.markdown("### AI Response")
+
     st.write(response.content)
